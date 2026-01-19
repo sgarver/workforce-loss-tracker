8928e8247c53f49bca42fdfb248cc393cce55f55
Stephen Garver <stepgar@gmail.com>
2026-01-19 09:33:36 -0800

    Test production CI: update log message

--- main.go
+++ main.go
@@ -309,7 +309,7 @@ func main() {
 		port = "8080"
 	}
 
-	log.Printf("Server starting on port %s - SDLC test update - build system test", port)
+	log.Printf("Server starting on port %s - SDLC test update - build system test - production check", port)
 	log.Fatal(e.Start("0.0.0.0:" + port))
 }
 
